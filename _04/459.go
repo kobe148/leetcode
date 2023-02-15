44f41221401d49bdf3bd63197af84e75bc8270a4
raolufeng <1400102825@qq.com>
2023-02-15 21:26:52 +0800

    04--459 2

--- _04/459.go
+++ _04/459.go
@@ -1,23 +1,29 @@
 package _04
 
 func repeatedSubstringPattern(s string) bool {
-	// 双指针模拟
+	// 双指针模拟: n 不能超过字符串的一半，n是length的倍数
+	// 程序要计算出子串有多少个字符
 	n := len(s)
+
+	// 尝试的子串长度
 	for length := 1; length*2 <= n; length++ {
-		if n%length == 0 {
-			matched := true
-			i := 0
-			for j := length; j < n; j++ {
-				if s[i] != s[j] {
-					matched = false
-					break
-				}
-				i++
+		if (n % length) != 0 {
+			continue
+		}
+		matched := true
+		i := 0
+		for j := length; j < n; j++ {
+			if s[i] != s[j] {
+				matched = false
+				break
 			}
-			if matched {
-				return true
-			}
+			i++
+		}
+
+		if matched {
+			return true
 		}
 	}
+
 	return false
 }
